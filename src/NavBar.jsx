72d2994a8bdea491340561a1f6e5406d0763d42e
Gabriel Pereira <gabriel_rjsouza@hotmail.com>
2023-03-21 23:55:09 -0300

    add provisoryTabble

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -14,11 +14,23 @@ function NavBar(){
         <Grid container style={{backgroundColor:'#1976d2', paddingLeft:50}} flexDirection='row'>
            
                
-            <Grid item container  alignItems={"center"} sx={{"&:hover":{backgroundColor:buttonBackColor}, height:'3.5vw', width:buttonWidth}}> <Link href="/" style={{width:buttonWidth, textAlign:'center'}}><Typography color={textButtonColor}> DroidWeb</Typography></Link> </Grid>
+           <Grid item container  alignItems={"center"} sx={{"&:hover":{backgroundColor:buttonBackColor}, height:'3.5vw', width:buttonWidth}}> 
+                <Link href="/" style={{width:buttonWidth, justifyContent:'center', height:'3.5vw', display:'flex',alignItems:'center'}}>
+                    <Typography color={textButtonColor}> DroidWeb</Typography>
+                </Link> 
+            </Grid>
 
-           <Grid item container alignItems={"center"} sx={{"&:hover":{backgroundColor:buttonBackColor}, height:'3.5vw', width:buttonWidth}}> <Link href="/gravar" style={{width:buttonWidth, textAlign:'center'}} ><Typography color={textButtonColor}> Gravar</Typography></Link> </Grid>
+           <Grid item container alignItems={"center"} sx={{"&:hover":{backgroundColor:buttonBackColor}, height:'3.5vw', width:buttonWidth}}> 
+                <Link href="/gravar" style={{width:buttonWidth, justifyContent:'center', height:'3.5vw', display:'flex',alignItems:'center'}} >
+                    <Typography color={textButtonColor}> Gravar</Typography>
+                </Link>
+            </Grid>
 
-           <Grid item container alignItems={"center"}sx={{"&:hover":{backgroundColor:buttonBackColor}, height:'3.5vw', width:buttonWidth}}>  <Link href="/medicoes" style={{width:buttonWidth, textAlign:'center'}}><Typography color={textButtonColor}> Medições</Typography></Link> </Grid>
+           <Grid item container alignItems={"center"}sx={{"&:hover":{backgroundColor:buttonBackColor}, height:'3.5vw', width:buttonWidth}}> 
+                <Link href="/medicoes" style={{width:buttonWidth, justifyContent:'center', height:'3.5vw', display:'flex',alignItems:'center'}}>
+                    <Typography color={textButtonColor} > Medições</Typography>
+                </Link> 
+            </Grid>
                     
            
         </Grid>
