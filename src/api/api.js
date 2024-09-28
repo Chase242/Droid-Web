c80c08bc2730650ab72a019ec28f77a4f1f83cb7
Gabriel Pereira <gabriel_rjsouza@hotmail.com>
2024-09-28 17:51:34 -0300

    update api link

--- src/api/api.js
+++ src/api/api.js
@@ -2,6 +2,6 @@ import axios from "axios"
 
 
 
-const api = axios.create({baseURL:'https://droidapi-v2.herokuapp.com',})
+const api = axios.create({baseURL:'https://droid-api-v2-main-lrm3ih5jw-gabriels-projects-567e018a.vercel.app',})
 
 export default api
